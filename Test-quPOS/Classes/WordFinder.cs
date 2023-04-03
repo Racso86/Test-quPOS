a010e5586007d967a202deb9841d906c75e574d7
Oscar Jemio <oscar.jemio@trueforce.com>
2023-04-03 14:58:10 -0400

    Master - Added more comments and little enhancements

--- Test-quPOS/Classes/WordFinder.cs
+++ Test-quPOS/Classes/WordFinder.cs
@@ -10,7 +10,7 @@ public class WordFinder
 
     public WordFinder(IEnumerable<string> matrix)
     {
-        this._matrix = matrix.Select(row => row.ToCharArray()).ToArray();
+        this._matrix = matrix.Select(row => row.ToLower().ToCharArray()).ToArray();
         this._numRows = matrix.Count();
         this._numCols = matrix.First().Length;
     }
@@ -19,13 +19,15 @@ public class WordFinder
     {
         // Build a hash set of unique words to search for
         // Applying Distinct in order to remove repeated words
-        HashSet<string> uniqueWords = new HashSet<string>(wordstream.Distinct());
+        // Using HashSet<T> instead of a List<T> to enhance performance for greater matrix dimensions
+        HashSet<string> uniqueWords = new HashSet<string>(wordstream.Select(word => word.ToLower()).Distinct());
 
-        // Initialize a dictionary to count the occurrences of each word
+        // Initialize a dictionary to count the occurrences of each word in wordstream
         Dictionary<string,int>? wordsFound = uniqueWords.ToDictionary(word => word, _ => 0); 
         // Search horizontally
         for (int i = 0; i < this._numRows; i++)
         {
+            //Creating a new string from the whole nth row
             string row = new string(_matrix[i]);
             foreach (string word in uniqueWords)
             {
@@ -39,6 +41,7 @@ public class WordFinder
         // Search vertically
         for (int j = 0; j < this._numCols; j++)
         {
+            //Creating a new string from the whole nth column
             string column = new string(_matrix.Select(row => row[j]).ToArray());
             foreach (string word in uniqueWords)
             {
